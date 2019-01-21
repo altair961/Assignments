bab884855d4b3b48110c2bf4298a3eb3b55ea1e9
altair961 <altair961@users.noreply.github.com>
2019-01-21 12:04:46 +0600

    Added unit tests

---
+++ Assignment1/AreaCalculator.UnitTests/AreaCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using AreaCalculator.Implementations;
+using Xunit;
+
+namespace AreaCalculator.UnitTests
+{
+    public class AreaCalculatorTests
+    {
+        [Theory]
+        [InlineData(3, 4, 5)]
+        public void When_InputProvided_Should_ReturnCorrectResult(int a, int b, int c)
+        {
+            var sut = new AreaCalculatorCommands();
+
+            var result = sut.CalculateArea(a, b, c);
+            Assert.True(result == 6);
+        }
+    }
+}
